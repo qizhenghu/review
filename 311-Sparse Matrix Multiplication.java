f9dea4bbff28172ddac71e333bba85405ccf8b6e
qizhenghu <springsun19@hotmail.com>
2018-05-08 20:52:46 -0400

    Update 311-Sparse Matrix Multiplication.java

--- 311-Sparse Matrix Multiplication.java
+++ 311-Sparse Matrix Multiplication.java
@@ -1,19 +1,17 @@
 class Solution {
     public int[][] multiply(int[][] A, int[][] B) {
-        int AR = A.length;
-        int AC = A[0].length;
-        int BR = B.length;
-        int BC = B[0].length;
-        int[][] result = new int[AR][BC];
-        for(int i=0; i< AR; i++){
-          for(int j=0; j<AC; j++){
-            if(A[i][j]!=0){
-              for(int k=0; k< BR; k++){
-                result[i][j] += B[k][j]*A[i][k];
-              }
+        int m = A.length, n = A[0].length, nB = B[0].length;
+        int[][] C = new int[m][nB];
+
+        for(int i = 0; i < m; i++) {
+            for(int k = 0; k < n; k++) {
+                if (A[i][k] != 0) {
+                    for (int j = 0; j < nB; j++) {
+                        if (B[k][j] != 0) C[i][j] += A[i][k] * B[k][j];
+                    }
+                }
             }
-          }
         }
-        return result;
+        return C;   
     }
 }
